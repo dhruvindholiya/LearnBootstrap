b7198c36f7f56ea11058d040359c155d7f5d078a
dhruvindholiya <dhruvindholiya1@gmail.com>
2023-03-17 19:18:55 +0530

    Had done desktop size of home page

---
+++ project/boodoo/assets/js/script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".client_slider", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        640: {
+            slidesPerView: 2,
+        },
+    },
+});
