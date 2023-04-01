bd8cb2d28d47832ab5bc3dc8627d4adb5db8ab42
dhruvindholiya <dhruvindholiya1@gmail.com>
2023-04-01 16:41:07 +0530

    made counter and main title animate

--- project/boodoo/assets/js/script.js
+++ project/boodoo/assets/js/script.js
@@ -1,11 +1,30 @@
-$(window).scroll(function() {    
+// add and remove class for header
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
     if (scroll >= 50) {
         $("body").addClass("header_scrolled");
     } else {
         $("body").removeClass("header_scrolled");
     }
-}); //missing ); 
+});
+
+// scroll page to top when click on the button
+$(document).ready(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+        if (scroll >= 100) {
+            $("#pageScrolled").addClass('show');
+
+        } else {
+            $("#pageScrolled").removeClass('show');
+        }
+    });
+    $("#pageScrolled").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        });
+    });
+});
 
 // client slider
 var swiper = new Swiper(".client_slider", {
@@ -22,3 +41,30 @@ var swiper = new Swiper(".client_slider", {
     },
 });
 
+
+const counterUp = window.counterUp.default
+
+const callback = entries => {
+    entries.forEach(entry => {
+        const el = entry.target
+        if (entry.isIntersecting && !el.classList.contains('is-visible')) {
+            counterUp(el, {
+                duration: 1000,
+                delay: 16,
+            })
+            el.classList.add('is-visible');
+        }
+    })
+}
+
+const IO = new IntersectionObserver(callback, { threshold: 0 })
+
+const el = document.querySelector('#partner');
+const el1 = document.querySelector('#teams');
+const el2 = document.querySelector('#happyClient');
+const el3 = document.querySelector('#serviced');
+
+IO.observe(el);
+IO.observe(el1);
+IO.observe(el2);
+IO.observe(el3);
